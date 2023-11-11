55afbc71ba1be0896441e490263ba2cc0e450a59
Fellipe Toledo de Azevedo <10598925708@rio.rj.gov.br>
2023-11-11 17:49:43 -0300

    feat: install nebular auth/security/theme/eva-icons dependencies. configure proxy/cors/routes

--- auth-app/src/app/app.module.ts
+++ auth-app/src/app/app.module.ts
@@ -3,16 +3,127 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { HttpClientModule } from '@angular/common/http';
+import { NbPasswordAuthStrategy, NbAuthModule } from '@nebular/auth';
+
+export interface NbAuthSocialLink {
+  link?: string,
+  url?: string,
+  target?: string,
+  title?: string,
+  icon?: string,
+}
+
+const socialLinks: NbAuthSocialLink[] = [];
+
+export const defaultSettings: any = {
+  forms: {
+    login: {
+      redirectDelay: 500, // delay before redirect after a successful login, while success message is shown to the user
+      strategy: 'email',  // strategy id key.
+      rememberMe: true,   // whether to show or not the `rememberMe` checkbox
+      showMessages: {     // show/not show success/error messages
+        success: true,
+        error: true,
+      },
+      socialLinks: socialLinks, // social links at the bottom of a page
+    },
+    register: {
+      redirectDelay: 500,
+      strategy: 'email',
+      showMessages: {
+        success: true,
+        error: true,
+      },
+      terms: true,
+      socialLinks: socialLinks,
+    },
+    requestPassword: {
+      redirectDelay: 500,
+      strategy: 'email',
+      showMessages: {
+        success: true,
+        error: true,
+      },
+      socialLinks: socialLinks,
+    },
+    resetPassword: {
+      redirectDelay: 500,
+      strategy: 'email',
+      showMessages: {
+        success: true,
+        error: true,
+      },
+      socialLinks: socialLinks,
+    },
+    logout: {
+      redirectDelay: 500,
+      strategy: 'email',
+    },
+    validation: {
+      password: {
+        required: true,
+        minLength: 4,
+        maxLength: 50,
+      },
+      email: {
+        required: true,
+      },
+      fullName: {
+        required: false,
+        minLength: 4,
+        maxLength: 50,
+      },
+    },
+  },
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    HttpClientModule,
+    NbAuthModule.forRoot({
+      strategies: [
+        NbPasswordAuthStrategy.setup({
+          name: 'email',
+
+          baseEndpoint: 'http://localhost:8080/api/v1',
+          login: {
+            // ...
+            endpoint: '/auth/sign-in',
+            method: 'post',
+          },
+          register: {
+            // ...
+            endpoint: '/auth/sign-up',
+            method: 'post',
+          },
+          logout: {
+            endpoint: '/auth/sign-out',
+            method: 'post',
+          },
+          requestPass: {
+            endpoint: '/auth/request-pass',
+            method: 'post',
+          },
+          resetPass: {
+            endpoint: '/auth/reset-pass',
+            method: 'post',
+          },
+        }),
+      ],
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
